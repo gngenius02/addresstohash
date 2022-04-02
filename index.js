519734d2d05e2ef6c4c555e0204ab75ab139012f
Nino <gngenius02@gmail.com>
2022-04-01 19:41:08 -0700

    code cleanup

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const { bech32 } = require('bech32');
-const base58 = require('bs58')
+const base58 = require('bs58');
+
+function int2Hex(num) {
+    return num.toString(16).padStart(2, 0);
+}
+
+
+function decodeBase58Address(address) {
+    const hexed = base58.decode(address);
+    return Buffer.from(hexed).toString('hex').slice(2, 42);
+}
+
+function decodeBech32Address(address) {
+    const hexed = bech32.decode(address).words.slice(1);
+    return bech32.fromWords(hexed).map(int2Hex).join('');
+}
 
 function decodeAddress(address) {
     if (address.startsWith('1') || address.startsWith('3')) {
-        const hexed = base58.decode(address);
-        return Buffer.from(hexed).toString('hex').slice(2, 42);
+        return decodeBase58Address(address);
     }
     if (address.startsWith('bc1')) {
-        const hexed = bech32.decode(address);
-        return bech32.fromWords(hexed.words.slice(1)).map(num => num.toString(16).padStart(2, 0)).join('')
+        return decodeBech32Address(address);
     }
 }
 
-const addresses = [
-    '159QgP4Ewvadjc4HDpaaR6pir2R4ZfzVfQ',
-    '3FupZp77ySr7jwoLYEJ9mwzJpvoNBXsBnE',
-    'bc1qx9t2l3pyny2spqpqlye8svce70nppwtaxwdrp4',
-    'bc1q9d4ywgfnd8h43da5tpcxcn6ajv590cg6d3tg6axemvljvt2k76zs50tv4q',
-];
+// const addresses = [
+//     '159QgP4Ewvadjc4HDpaaR6pir2R4ZfzVfQ',
+//     '3FupZp77ySr7jwoLYEJ9mwzJpvoNBXsBnE',
+//     'bc1qx9t2l3pyny2spqpqlye8svce70nppwtaxwdrp4',
+//     'bc1q9d4ywgfnd8h43da5tpcxcn6ajv590cg6d3tg6axemvljvt2k76zs50tv4q',
+// ];
+// const decoded = addresses.map(decodeAddress);
+// console.log(decoded)
 
 
-const decoded = addresses.map(decodeAddress);
 
-console.log(decoded)
